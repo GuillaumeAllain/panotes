d44b60f7a854d80015abf2ba38b7f303ed3e82d4
GuillaumeAllain <13963457+GuillaumeAllain@users.noreply.github.com>
2021-09-10 11:33:11 -0400

    big update for diary and capture

--- lua/panotes/init.lua
+++ lua/panotes/init.lua
@@ -3,15 +3,15 @@ local scandir = require("plenary.scandir")
 local _show_tags = require("telescope.builtin").tags
 local m = {}
 
-local function _change_cwd_to_notes_dir()
+function m.change_cwd_to_notes_dir()
 	vim.cmd("e" .. vim.fn.expand("$NOTES_DIR/") .. ".notes")
 end
 
-local function _opentemppandocbuff(loctable, opt)
-	api.nvim_command("silent enew") -- equivalent to :enew
-	api.nvim_buf_set_lines(buf, 0, -1, false, loctable)
+local function _opentemppandocbuff(loctable)
+	api.nvim_command("silent enew")
+	api.nvim_buf_set_lines(0, 0, -1, false, loctable)
 	vim.bo[0].filetype = "pandoc"
-	vim.bo[0].buftype = "nofile" -- set the current buffer's (buffer 0) buftype to nofile
+	vim.bo[0].buftype = "nofile"
 	vim.bo[0].bufhidden = "delete"
 	vim.bo[0].swapfile = false
 	vim.bo[0].readonly = true
@@ -70,7 +70,7 @@ local function _get_datetime_from_journal_filename(path)
 	return os.time({ year = jyear, day = jday, month = jmonth })
 end
 
-local function _opennewdiary(todayfile)
+local function _newdiary_content()
 	local rfile = io.open(os.getenv("HOME") .. "/.local/share/pandot/templates/documents/diary.md", "r")
 	local templatetable = {}
 	local filename = vim.fn.system("id -F"):gsub("\n", "")
@@ -89,18 +89,58 @@ local function _opennewdiary(todayfile)
 		end
 		templatetable[#templatetable + 1] = locline
 	end
-	api.nvim_command("e " .. todayfile)
-	api.nvim_buf_set_lines(buf, 0, -1, false, templatetable)
+	return templatetable
+end
+
+local function _todayfile()
+	return vim.fn.expand("$NOTES_DIR/journal" .. "/" .. os.date("%Y%m%d.md"))
+end
+
+local function _open_file_buffer(filename)
+	local buffer_number = nil
+	local buffer_list = vim.fn.getbufinfo()
+	local already_open = false
+
+	for k in pairs(buffer_list) do
+		if
+			vim.fn.fnamemodify(vim.fn.bufname(buffer_list[k].bufnr), ":p") == vim.fn.resolve(vim.fn.expand(filename))
+		then
+			buffer_number = vim.fn.deepcopy(buffer_list[k].bufnr)
+			already_open = true
+			break
+		end
+	end
+
+	if not already_open then
+		buffer_number = vim.api.nvim_create_buf(true, false)
+		vim.api.nvim_buf_set_name(buffer_number, vim.fn.expand(filename))
+		vim.api.nvim_buf_set_option(buffer_number, "filetype", "pandoc")
+		if vim.fn.filereadable(filename) == 1 then
+			vim.api.nvim_buf_call(buffer_number, function()
+				vim.cmd("silent exec ':e|w!'")
+			end)
+		end
+	end
+	return { number = buffer_number, state = already_open }
+end
+
+local function _open_diary_buffer()
+	local template_flag = false
+	local todayfile = _todayfile()
+	if vim.fn.filereadable(todayfile) == 0 then
+		template_flag = true
+	end
+	local buffer_info = _open_file_buffer(todayfile)
+	if template_flag then
+		vim.api.nvim_buf_set_lines(buffer_info.number, 0, -1, false, _newdiary_content())
+	end
+	return buffer_info
 end
 
 function m.openDiary()
-	local todayfile = vim.fn.expand("$NOTES_DIR/journal" .. "/" .. os.date("%Y%m%d.md"))
-	print(todayfile)
-	if vim.fn.filereadable(todayfile) == 0 then
-		_opennewdiary(todayfile)
-	else
-		api.nvim_command("e " .. todayfile)
-	end
+	local buffer_info = _open_diary_buffer()
+	vim.api.nvim_exec("buffer " .. buffer_info.number, {})
+	vim.api.nvim_call_function("pandoc#folding#Init", {})
 end
 
 function m.openJournal()
@@ -123,13 +163,14 @@ function m.openJournal()
 			filetable[#filetable + 1] = " "
 		end
 		datestring = os.date("%A le %d", datetime)
-		filetable[#filetable + 1] = datestring .. ":" .. string.rep(" ", 15 - #datestring) .. filename
+		filetable[#filetable + 1] = datestring .. string.rep(" ", 15 - #datestring) .. "journal/" .. filename
 	end
+	m.change_cwd_to_notes_dir()
 	_opentemppandocbuff(filetable, { directory = vim.fn.expand("$NOTES_DIR/journal") })
 end
 
 function m.openTagInput()
-	_change_cwd_to_notes_dir()
+	m.change_cwd_to_notes_dir()
 	local taginput = vim.fn.input({ prompt = "Tag to search: ", completion = "custom,v:lua.Panotes_tags" })
 	if taginput == nil then
 		return
@@ -138,7 +179,7 @@ function m.openTagInput()
 end
 
 function m.searchTags()
-	_change_cwd_to_notes_dir()
+	m.change_cwd_to_notes_dir()
 	_show_tags({ ctags_file = vim.fn.tagfiles()[1] })
 end
 
@@ -163,6 +204,149 @@ local function make_commands()
 	vim.cmd(
 		[[command! -nargs=* -complete=custom,v:lua.Panotes_complete Panotes    lua require('panotes').load_command(<f-args>)]]
 	)
+end
+
+function m.get_capture_buffer()
+	-- Returns build terminal handle. If not found, it creates it
+	local buffer_list = vim.fn.getbufinfo()
+	local create_buffer = true
+	local buffer_number = nil
+
+	for k in pairs(buffer_list) do
+		if vim.fn.bufname(buffer_list[k].bufnr) == "PanotesCapture" then
+			buffer_number = vim.fn.deepcopy(buffer_list[k].bufnr)
+			create_buffer = false
+			break
+		end
+	end
+
+	if create_buffer then
+		buffer_number = vim.api.nvim_create_buf(false, true)
+
+		-- Set the buffer type to "prompt" to give it special behaviour (:h prompt-buffer)
+		vim.api.nvim_buf_set_option(buffer_number, "buftype", "nofile")
+		vim.api.nvim_buf_set_option(buffer_number, "ft", "pandoc")
+		vim.api.nvim_buf_set_name(buffer_number, "PanotesCapture")
+	end
+
+	return buffer_number
+end
+
+function m.get_capturename_buffer()
+	-- Returns build terminal handle. If not found, it creates it
+	local buffer_list = vim.fn.getbufinfo()
+	local create_buffer = true
+	local buffer_number = nil
+
+	for k in pairs(buffer_list) do
+		if vim.fn.bufname(buffer_list[k].bufnr) == "PanotesCaptureName" then
+			buffer_number = vim.fn.deepcopy(buffer_list[k].bufnr)
+			create_buffer = false
+			break
+		end
+	end
+
+	if create_buffer then
+		buffer_number = vim.api.nvim_create_buf(false, false)
+
+		-- Set the buffer type to "prompt" to give it special behaviour (:h prompt-buffer)
+		vim.api.nvim_buf_set_option(buffer_number, "buftype", "nofile")
+		vim.api.nvim_buf_set_name(buffer_number, "PanotesCaptureName")
+		vim.api.nvim_buf_set_lines(buffer_number, 0, -1, false, { "  Panotes Capture " })
+	end
+
+	return buffer_number
+end
+
+local function _append_to_buffer(buffer_info, lines)
+	vim.api.nvim_buf_set_lines(buffer_info.number, -1, -1, false, lines)
+	vim.api.nvim_buf_call(buffer_info.number, function()
+		vim.cmd("silent exec 'w'")
+	end)
+	if not buffer_info.state then
+		vim.api.nvim_buf_delete(buffer_info.number, {})
+	end
+end
+
+function _G.Panotes_folders_complete(arglead, cmdline, cursorpos)
+	local output = {}
+	local current_dir = vim.fn.resolve(vim.fn.expand("$NOTES_DIR/"))
+	local current_dir_arg = vim.fn.resolve(vim.fn.expand("$NOTES_DIR/" .. arglead))
+	if vim.fn.isdirectory(current_dir_arg) == 1 then
+		output = scandir.scan_dir(current_dir_arg, { only_dirs = true, depth = 1 })
+		for index, _ in ipairs(output) do
+			output[index] = arglead .. (string.gsub(output[index], current_dir_arg .. "/", ""))
+		end
+	elseif vim.fn.isdirectory(current_dir) == 1 then
+		local input = scandir.scan_dir(current_dir, { only_dirs = true, depth = 1 })
+		for index, _ in ipairs(input) do
+			input[index] = (string.gsub(input[index], current_dir .. "/", ""))
+			if input[index]:find(arglead) then
+				output[#output + 1] = input[index]
+			end
+		end
+	end
+	return api.nvim_call_function("join", { output, "\n" })
+end
+
+function m.close_capture(buf, bufname, winname, winnamename)
+	local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+	table.insert(lines, 1, "")
+	vim.api.nvim_buf_delete(bufname, {})
+	vim.api.nvim_buf_delete(buf, {})
+	vim.api.nvim_command("redraw")
+	vim.cmd([[
+        augroup PanotesCapture
+        au!
+        augroup END
+        ]])
+	local file_decision = vim.fn.input({
+		prompt = "Where to save?:\n\t (1) Append to diary\n\t (2) Append to note\n\t (3) Discard \n:",
+	})
+	vim.api.nvim_command("redraw")
+	if file_decision == "1" then
+		local buffer_info = _open_diary_buffer()
+		_append_to_buffer(buffer_info, lines)
+	elseif file_decision == "2" then
+		local filename = vim.fn.input({
+			prompt = "Filename?:\n:",
+			completion = "custom,v:lua.Panotes_folders_complete",
+		})
+		vim.api.nvim_command("redraw")
+		local buffer_info = _open_file_buffer(vim.fn.expand("$NOTES_DIR/") .. filename .. ".md")
+		_append_to_buffer(buffer_info, lines)
+	end
+end
+
+function m.capture()
+	local window_config = {
+		relative = "editor",
+		style = "minimal",
+		border = "rounded",
+		width = vim.fn.ceil(vim.o.columns / 2),
+		height = vim.fn.ceil(vim.o.lines / 10),
+		col = vim.fn.ceil((vim.o.columns / 2) - (vim.o.columns / 4)) + vim.o.cmdheight,
+		row = vim.fn.ceil((vim.o.lines / 2) - (vim.o.lines / 20)),
+	}
+	local window_name_config = {
+		relative = "editor",
+		style = "minimal",
+		width = 9,
+		height = 1,
+		col = window_config.col + 2,
+		row = window_config.row,
+		zindex = 100,
+	}
+
+	local buf = m.get_capture_buffer()
+	local bufname = m.get_capturename_buffer()
+	local window_name = vim.api.nvim_open_win(buf, true, window_config)
+	local window_name_name = vim.api.nvim_open_win(bufname, false, window_name_config)
+	vim.cmd([[augroup panotescapture
+	    au!
+	autocmd WinClosed,BufDelete,WinLeave <buffer=]] .. buf .. "> ++once lua require'panotes'.close_capture(" .. buf .. "," .. bufname .. [[)
+        augroup END]])
+	vim.api.nvim_feedkeys("i", "t", false)
 end
 
 function m.setup() end
