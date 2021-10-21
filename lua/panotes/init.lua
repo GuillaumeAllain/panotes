52e052b1ad4229acfc9b6df8e9c7d1839620f24b
GuillaumeAllain <13963457+GuillaumeAllain@users.noreply.github.com>
2021-10-20 23:22:51 -0400

    correct autocommand and make sure autocompletion is available

--- lua/panotes/init.lua
+++ lua/panotes/init.lua
@@ -1,8 +1,15 @@
 local m = {}
 
+function _G.Panotes_complete(arglead, cmdline, cursorpos)
+    return vim.aapi.nvim_call_function(
+        "join",
+        { { "openDiary", "openJournal", "openTagInput", "searchTags"}, "\n" }
+    )
+end
+
 local function make_commands()
     vim.cmd(
-        [[command! -nargs=* -complete=custom,v:lua.Panotes_complete Panotes    lua require('panotes.command').load_command(<f-args>)]]
+        [[command! -nargs=* -complete=custom,v:lua.Panotes_complete Panotes    lua require('panotes.commands').load_command(<f-args>)]]
     )
 end
 
